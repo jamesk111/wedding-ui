4b5c3b02be1a9753d060cf123e99fac32a3abe6c
jamesk111 <8887227+jamesk111@users.noreply.github.com>
2020-08-15 13:40:15 -0600

    Add nav and landing components w/ routing & global styles for background

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {LandingComponent} from './components/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: 'home', component: LandingComponent},
+	// {path: 'nav', component: NavComponent},
+	{path: '**', redirectTo: 'home'}
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
