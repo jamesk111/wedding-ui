1611f73c2ee425ade4c648f33cdd735969586db7
jamesk111 <8887227+jamesk111@users.noreply.github.com>
2020-08-15 18:26:43 -0600

    Add Party & Registry pages; Style Nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LandingComponent} from './components/landing/landing.component';
+import {PartyComponent} from './components/party/party.component';
+import {RegistryComponent} from './components/registry/registry.component';
 
 const routes: Routes = [
-	{path: 'home', component: LandingComponent},
-	// {path: 'nav', component: NavComponent},
-	{path: '**', redirectTo: 'home'}
+	{path: 'details', component: LandingComponent},
+	{path: 'party', component: PartyComponent},
+	{path: 'registry', component: RegistryComponent},
+	{path: '**', redirectTo: 'details'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { NavComponent } from './components/nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { PartyComponent } from './components/party/party.component';
+import { RegistryComponent } from './components/registry/registry.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		LandingComponent,
-		NavComponent
+		NavComponent,
+		PartyComponent,
+		RegistryComponent,
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatToolbarModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
