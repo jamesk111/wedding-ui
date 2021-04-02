de1ce6f2abacd1972c8b7999c21d13d0355b6e10
jamesk111 <8887227+jamesk111@users.noreply.github.com>
2021-04-01 19:35:56 -0600

    Code cleanup

--- src/app/components/callback/callback.component.ts
+++ src/app/components/callback/callback.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-callback',
-  templateUrl: './callback.component.html',
-  styleUrls: ['./callback.component.scss']
+	selector: 'app-callback',
+	templateUrl: './callback.component.html',
+	styleUrls: ['./callback.component.scss']
 })
 export class CallbackComponent implements OnInit {
 
-  constructor() { }
+	constructor() {
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
